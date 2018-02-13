976983178b99321f2da75fc7455369aa63ac6bc5
Saveli <grindhouse210@icloud.com>
2018-02-13 13:40:49 +0300

    Изменения
    Изменения

--- index.php
+++ index.php
@@ -29,23 +29,6 @@ $is_auth;
 
         <nav class="user-menu">
 
-<? if ($is_auth = true): ?>
-<div class="user-menu__image">
-    <img src="<?=$user_avatar?>" width="40" height="40" alt="Пользователь">
-  </div>
-  <div class="user-menu__logged">
-    <p><?=$user_name?></p>
-  </div>
-<? else: ?>
-<ul class="user-menu__list">
-    <li class="user-menu__item">
-      <a href="#">Регистрация</a>
-    </li>
-    <li class="user-menu__item">
-      <a href="#">Вход</a>
-    </li>
-  </ul>
-<? endif; ?>
 
        </nav>
     </div>
