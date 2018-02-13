22e6c2c450fe8c968d4be8710558eaf601c73bbe
Saveli <grindhouse210@icloud.com>
2018-02-13 13:09:52 +0300

    Задание 1
    Задание 1

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+$is_auth;
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -28,9 +29,25 @@ $user_avatar = 'img/user.jpg';
 
         <nav class="user-menu">
 
-        <!-- здесь должен быть PHP код для показа аватара пользователя -->
+<? if ($is_auth = true): ?>
+<div class="user-menu__image">
+    <img src="<?=$user_avatar?>" width="40" height="40" alt="Пользователь">
+  </div>
+  <div class="user-menu__logged">
+    <p><?=$user_name?></p>
+  </div>
+<? else: ?>
+<ul class="user-menu__list">
+    <li class="user-menu__item">
+      <a href="#">Регистрация</a>
+    </li>
+    <li class="user-menu__item">
+      <a href="#">Вход</a>
+    </li>
+  </ul>
+<? endif; ?>
 
-        </nav>
+       </nav>
     </div>
 </header>
 
