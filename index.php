b337f9b0265cdfc9b4b147a5c27e79256b4780f5
Saveli <grindhouse210@icloud.com>
2018-02-19 17:22:51 +0300

    4/4

--- index.php
+++ index.php
@@ -7,39 +7,40 @@ $x = 0;
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
 $is_auth;
-$category = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+$category = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
       $items = [
-                 ['name' => '2014 Rossignol District Snowboard',
+                 'one' => ['name' => '2014 Rossignol District Snowboard',
                   'category' => 'Доски и лыжи',
                   'price' => 10999,
                   'url' => 'img/lot-1.jpg'
                  ],
-                 ['name' => 'DC Ply Mens 2016/2017 Snowboard',
+                 'two' => ['name' => 'DC Ply Mens 2016/2017 Snowboard',
                   'category' => 'Доски и лыжи',
                   'price' => 159999,
                   'url' => 'img/lot-2.jpg'
                  ],
-                 ['name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+                 'three' => ['name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
                  'category' => 'Крепления',
                  'price' => 8000,
                  'url' => 'img/lot-3.jpg'
                  ],
-                 ['name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+                 'four' => ['name' => 'Ботинки для сноуборда DC Mutiny Charocal',
                  'category' => 'Ботинки',
                  'price' => 10999,
                  'url' => 'img/lot-4.jpg'
                  ],
-                 ['name' => 'Куртка для сноуборда DC Mutiny Charocal',
+                 'five' => ['name' => 'Куртка для сноуборда DC Mutiny Charocal',
                  'category' => 'Одежда',
                  'price' => 7500,
                  'url' => 'img/lot-5.jpg'
                  ],
-                 ['name' => 'Маска Oakley Canopy',
+                 'six' => ['name' => 'Маска Oakley Canopy',
                  'category' => 'Разное',
                  'price' => 5400,
                  'url' => 'img/lot-6.jpg']
-                ];
-$amount = count($categories);
+               ];
+$amount = count($category);
+$amount_items = count($items);
 
 ?>
 <!DOCTYPE html>
@@ -97,24 +98,13 @@ $amount = count($categories);
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="promo__item promo__item--attachment">
-                <a class="promo__link" href="all-lots.html">Крепления</a>
-            </li>
-            <li class="promo__item promo__item--boots">
-                <a class="promo__link" href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="promo__item promo__item--clothing">
-                <a class="promo__link" href="all-lots.html">Одежда</a>
-            </li>
-            <li class="promo__item promo__item--tools">
-                <a class="promo__link" href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="promo__item promo__item--other">
-                <a class="promo__link" href="all-lots.html">Разное</a>
-            </li>
+
+<?php while ($x < $amount) : ?>
+      <li class="promo__item promo__item--attachment">
+          <a class="promo__link" href="all-lots.html"><?=$category[$x]?></a>
+      </li>
+     <?php $x = $x + 1; ?>
+<?php endwhile; ?>
         </ul>
     </section>
     <section class="lots">
@@ -122,17 +112,18 @@ $amount = count($categories);
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+      <?php foreach ($items as $key => $value) : ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=$value['url']?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$value['category']?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$value['name']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$value['price']?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -140,8 +131,10 @@ $amount = count($categories);
                     </div>
                 </div>
             </li>
-        </ul>
-    </section>
+     <?php endforeach; ?>
+      </ul>
+
+ </section>
 </main>
 
 <footer class="main-footer">
@@ -150,7 +143,7 @@ $amount = count($categories);
 
       <?php while ($x < $amount) : ?>
             <li class="nav__item">
-                <a href="all-lots.html">'<?=$category[$x]?></a>
+                <a href="all-lots.html"><?=$category[$x]?></a>
             </li>
            <?php $x = $x + 1; ?>
       <?php endwhile; ?>
